43bbd0e0fb04d16ff5da7e8c38f0c0ffeb80cade
Tomasz Jarzabek <tomaszjarzabek@MacBook-Pro-di-Tomasz.local>
2023-03-13 21:05:56 +0100

    Initial commit

---
+++ src/Source.java
@@ -0,0 +1,10 @@
+//Tomasz Jarzabek - 5
+
+import java.util.Scanner;
+class Source {
+
+    public static Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
